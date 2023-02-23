00dcbd31227b2c78f12e439ac5e7cbffbe2e2038
strayneko <bayurendi10@gmail.com>
2023-02-23 14:55:57 +0700

    :sparkle: Episode 34 - Notify All Voters

--- app/Http/Livewire/SetStatus.php
+++ app/Http/Livewire/SetStatus.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Mail\IdeaStatusUpdateMailable;
 use App\Models\Idea;
+use Illuminate\Support\Facades\Mail;
 use Livewire\Component;
 
 class SetStatus extends Component
 {
     public $idea;
     public $status;
+    public $notifyAllVoters;
 
     public function mount(Idea $idea)
     {
@@ -24,7 +27,24 @@ class SetStatus extends Component
         $this->idea->save();
 
         $this->emit('statusWasUpdated');
+
+        if ($this->notifyAllVoters) {
+            $this->notifyAllVoters();
+        }
     }
+
+    public function notifyAllVoters()
+    {
+        $this->idea->votes()
+            ->select('name', 'email')
+            ->chunk(100, function ($voters) {
+                foreach ($voters as $user) {
+                    Mail::to($user)
+                        ->queue(new IdeaStatusUpdateMailable($this->idea));
+                }
+            });
+    }
+
     public function render()
     {
         return view('livewire.set-status');
--- resources/views/livewire/set-status.blade.php
+++ resources/views/livewire/set-status.blade.php
@@ -73,15 +73,16 @@
                                 <span class="ml-1">Attach</span>
                             </button>
                             <button type="submit"
-                                class="flex items-center justify-center w-1/2 h-11 text-xs bg-theme-blue-primary text-white font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3">
+                                class="flex disabled:opacity-50 items-center justify-center w-1/2 h-11 text-xs bg-theme-blue-primary text-white font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3"
+                                wire:load='disabled'>
                                 <span class="ml-1">Update</span>
                             </button>
                         </div>
 
                         <div>
                             <label class="font-normal inline-flex items-center cursor-pointer">
-                                <input type="checkbox" name="notify_voters" class="rounded cursor-pointer bg-gray-200"
-                                    checked="">
+                                <input wire:model='notifyAllVoters' type="checkbox" name="notify_voters"
+                                    class="rounded cursor-pointer bg-gray-200" checked="">
                                 <span class="ml-2">Notify all voters</span>
                             </label>
                         </div>
